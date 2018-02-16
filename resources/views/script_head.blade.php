61ba0e40e844ce1af6ad1d46f2dfde391834a05e
angallego <angel.gallego@grupo-talentum.com>
2018-02-16 11:40:59 +0100

    Update script_head.blade.php
    Mocking PageSpeed Insights checks

--- resources/views/script_head.blade.php
+++ resources/views/script_head.blade.php
@@ -1,18 +1,21 @@
-<?php
-$dataLayer = GoogleTagManager::getDataLayer();
-if(!isset($pushData))$pushData = array(); ?>
-<script>
+<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
+
+    <?php
+    $dataLayer = GoogleTagManager::getDataLayer();
+    if(!isset($pushData))$pushData = array(); ?>
+    <script>
     window.dataLayer = window.dataLayer || [];
     dataLayer = [{!! $dataLayer->toJson() !!}];
     @foreach($pushData as $item)
-        dataLayer.push({!! $item->toJson() !!});
+    dataLayer.push({!! $item->toJson() !!});
     @endforeach
 </script>
-
-
 
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer',"{{config('googletagmanager.id')}}");</script>
+
+
+<?php endif; ?>
