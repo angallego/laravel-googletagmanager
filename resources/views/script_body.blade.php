513a8b44b16997089ef6b67bef0e1f4c7ea1261b
angallego <angel.gallego@grupo-talentum.com>
2018-02-16 11:40:36 +0100

    Update script_body.blade.php
    Mocking PageSpeed Insights checks

--- resources/views/script_body.blade.php
+++ resources/views/script_body.blade.php
@@ -1,3 +1,7 @@
+<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
+
 <noscript>
     <iframe src="//www.googletagmanager.com/ns.html?id={{config('googletagmanager.id')}}" height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
+
+<?php endif; ?>
